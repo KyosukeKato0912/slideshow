89eb363bbf2bec1b766d6d800f9024b58d82be06
KyosukeKato0912 <katou.kyosuke0912@gmail.com>
2026-06-22 21:52:15 +0900

    コミットテスト

--- lib/core/config/habit_config.dart
+++ lib/core/config/habit_config.dart
@@ -61,7 +61,7 @@ abstract class HabitConfig {
     return null;
   }
 
-  /// 花丸画像IDからアセットパスを返す。テスト
+  /// 花丸画像IDからアセットパスを返す。
   static String flowerCircleAssetPath(String id) =>
       '$flowerCircleAssetFolder$id.webp';
 
